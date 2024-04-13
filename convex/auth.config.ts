ec0156ab6b9b3b04814b4798838854023824ad12
hiropi1224 <hiropi1224s@gmail.com>
2024-04-13 17:03:27 +0900

    🔧 convex clerk config

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+const authConfig = {
+  providers: [
+    {
+      domain: "https://game-mackerel-87.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
+
+export default authConfig;
