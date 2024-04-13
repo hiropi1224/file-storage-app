9c1e1134bef91200e1eaeb9a6c71d6efccc7927d
hiropi1224 <hiropi1224s@gmail.com>
2024-04-13 23:28:25 +0900

    👔 convex function / schema

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,20 @@
+"use client";
+
 import {
   SignInButton,
   SignOutButton,
   SignedIn,
   SignedOut,
 } from "@clerk/nextjs";
+import { useMutation, useQuery } from "convex/react";
 
 import { Button } from "~/components/ui/button";
+import { api } from "~/convex/_generated/api";
 
 export default function Home() {
+  const files = useQuery(api.files.getFiles);
+  const createFile = useMutation(api.files.createFile);
+
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-24'>
       <SignedIn>
@@ -20,6 +27,12 @@ export default function Home() {
           <Button>Sign In</Button>
         </SignInButton>
       </SignedOut>
+
+      {files?.map((file) => <div key={file._id}>{file.name}</div>)}
+
+      <Button onClick={() => createFile({ name: "hello world" })}>
+        Click Me
+      </Button>
     </main>
   );
 }
---
+++ convex/files.ts
@@ -0,0 +1,33 @@
+import { v } from "convex/values";
+
+import { mutation, query } from "./_generated/server";
+
+export const createFile = mutation({
+  args: {
+    name: v.string(),
+  },
+  async handler(ctx, args) {
+    const identity = await ctx.auth.getUserIdentity();
+
+    if (!identity) {
+      throw new Error("you must be logged in upload a file");
+    }
+
+    await ctx.db.insert("files", {
+      name: args.name,
+    });
+  },
+});
+
+export const getFiles = query({
+  args: {},
+  async handler(ctx, _args) {
+    const identity = await ctx.auth.getUserIdentity();
+
+    if (!identity) {
+      return [];
+    }
+
+    return ctx.db.query("files").collect();
+  },
+});
