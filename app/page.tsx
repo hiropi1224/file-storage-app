b4e1822c90c3d2fae6d8f4a31cf04fae124d572b
hiropi1224 <hiropi1224s@gmail.com>
2024-04-13 17:13:18 +0900

    💄 shadcn / clerk component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,25 @@
+import {
+  SignInButton,
+  SignOutButton,
+  SignedIn,
+  SignedOut,
+} from "@clerk/nextjs";
+
+import { Button } from "~/components/ui/button";
+
 export default function Home() {
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-      Hello, World
+      <SignedIn>
+        <SignOutButton>
+          <Button>Sign Out</Button>
+        </SignOutButton>
+      </SignedIn>
+      <SignedOut>
+        <SignInButton mode='modal'>
+          <Button>Sign In</Button>
+        </SignInButton>
+      </SignedOut>
     </main>
   );
 }
